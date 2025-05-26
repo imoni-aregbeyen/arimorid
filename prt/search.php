72c8a1f2b3d95b701eaa78c001d957efa820a3fd
Imonitie Aregbeyen <aregbeyenimonitie@gmail.com>
2025-05-26 12:30:42 +0100

    home search app ui

---
+++ prt/search.php
@@ -0,0 +1,25 @@
+        <div class="container-fluid bg-primary mb-5 wow fadeIn" data-wow-delay="0.1s" style="padding: 35px;">
+            <div class="container">
+                <div class="row g-2">
+                    <div class="col-md-12">
+                        <div class="row g-2">
+                            <div class="col-md-4">
+                              <div class="input-group">
+                                <label for="checkIn" class="form-control">Check In</label>
+                                <input type="date" name="check_in" id="checkIn" class="form-control" required>
+                              </div>
+                            </div>
+                            <div class="col-md-4">
+                              <div class="input-group">
+                                <label for="checkOut" class="form-control">Check Out</label>
+                                <input type="date" name="check_out" id="checkOut" class="form-control" required>
+                              </div>
+                            </div>
+                            <div class="col-md-4">
+                              <button type="submit" class="btn btn-dark w-100">Search Availability</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
