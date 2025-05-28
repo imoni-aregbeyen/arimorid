14dff8458d7927639dea58cd853ad8e2953d2f25
Imonitie Aregbeyen <aregbeyenimonitie@gmail.com>
2025-05-28 18:30:30 +0100

    services page content added

---
+++ pg/services.php
@@ -0,0 +1,27 @@
+        <div class="container-xxl py-5">
+            <div class="container">
+                <div class="row g-5 align-items-center">
+                    <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                        <h1 class="mb-4">#1 Property Sales & Leasing</h1>
+                        <p><i class="fa fa-check text-primary me-3"></i>Residential homes, luxury apartments, & commercial spaces</p>
+                        <p><i class="fa fa-check text-primary me-3"></i>Nationwide portfolio with exclusive listings</p>
+                    </div>
+                    <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                        <h1 class="mb-4">#2 Property Management</h1>
+                        <p><i class="fa fa-check text-primary me-3"></i>Tenant screening & rent collection</p>
+                        <p><i class="fa fa-check text-primary me-3"></i>Maintenance coordination & financial reporting</p>
+                    </div>
+                    <div class="col-lg-6 wow fadeIn" data-wow-delay="0.9s">
+                        <h1 class="mb-4">#3 Real Estate Training</h1>
+                        <p><i class="fa fa-check text-primary me-3"></i>Landlord best practices</p>
+                        <p><i class="fa fa-check text-primary me-3"></i>Real estate investing fundamentals</p>
+                        <p><i class="fa fa-check text-primary me-3"></i>Agent certification programs</p>
+                    </div>
+                    <div class="col-lg-6 wow fadeIn" data-wow-delay="1.3s">
+                        <h1 class="mb-4">#4 Consultancy & Valuation</h1>
+                        <p><i class="fa fa-check text-primary me-3"></i>Market research & feasibility studies</p>
+                        <p><i class="fa fa-check text-primary me-3"></i>Property appraisals & strategic advisory</p>
+                    </div>
+                </div>
+            </div>
+        </div>
